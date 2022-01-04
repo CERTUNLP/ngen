969d2511d790d8323d8943927f4ee6844ada7645
dam <drubio@cert.unlp.edu.ar>
2022-01-03 23:01:12 -0300

    docker env var and images refactor
    Signed-off-by: dam <drubio@cert.unlp.edu.ar>

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -149,5 +149,5 @@ CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL')
 #: Only add pickle to this list if your broker is secured
 #: from unwanted access (see userguide/security.html)
 CELERY_ACCEPT_CONTENT = ['json']
-CELERY_RESULT_BACKEND = 'rpc://guest:guest@ngen-rabbitmq:5672'
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND')
 CELERY_TASK_SERIALIZER = 'json'
