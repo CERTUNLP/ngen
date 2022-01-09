6bc4a47aeafca8871b281c24ed52da6049cd417e
dam <drubio@cert.unlp.edu.ar>
2022-01-09 13:35:49 -0300

    translatios enable
    Signed-off-by: dam <drubio@cert.unlp.edu.ar>

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from django.utils.translation import gettext_lazy as _
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware'
 ]
 
 ROOT_URLCONF = 'djangoProject.urls'
@@ -127,7 +130,10 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
+LANGUAGES = (
+    ('en-us', _('English')),
+    ('es', _('Spanish')),
+)
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
