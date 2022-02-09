2b8abaa7be0ecdc4a2bf4b1e1b60d5dd141ef87f
dam <drubio@cert.unlp.edu.ar>
2022-02-09 17:16:37 -0300

    docker env entrypoint fixes
    Signed-off-by: dam <drubio@cert.unlp.edu.ar>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-pip install -r requirements.txt
 python manage.py makemessages -l es -i venv --no-location
 python manage.py compilemessages -l es -i venv
 python manage.py migrate --noinput
