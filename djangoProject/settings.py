563279d08d132b84715051a1d8e24ff474eed6a1
dam <drubio@cert.unlp.edu.ar>
2022-04-20 19:43:41 -0300

    changed 0.0.0.0 for localhost on docker-compose.yml
    Signed-off-by: dam <drubio@cert.unlp.edu.ar>

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-#j!42e(tj8h#n&nl#cxg#(lu=j9=(pcf*=tep$qv%@1^yst4!*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
