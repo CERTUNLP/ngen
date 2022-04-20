499726d0d0bece1ea73964cc20d04a885ce48a44
dam <drubio@cert.unlp.edu.ar>
2022-04-20 13:16:36 -0300

    admin renderer for frontend
    Signed-off-by: dam <drubio@cert.unlp.edu.ar>

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -130,6 +130,11 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.AdminRenderer',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
