4246a5a19653ed48e30afff352cebef72967f0bf
dam <drubio@cert.unlp.edu.ar>
2022-05-01 17:55:06 -0300

    artifacts relation and querys #7
    Signed-off-by: dam <drubio@cert.unlp.edu.ar>

--- ngen/models/artifact.py
+++ ngen/models/artifact.py
@@ -11,28 +11,45 @@ class Artifact(NgenModel):
     TYPE = Choices(('ip', 'IP'), ('domain', gettext_lazy('Domain')), (
         'url', 'Url'), ('hash', 'Hash'), ('file', gettext_lazy('File')), ('domain', gettext_lazy('Domain')))
     type = models.CharField(choices=TYPE, default=TYPE.ip, max_length=20)
-    value = models.TextField()
+    value = models.CharField(blank=False, null=False, max_length=255, default=None)
+
+    class Meta:
+        db_table = 'artifact'
 
     def __str__(self):
-        display = self.value
-        if self.targets.count() > 1:
-            display += " (%s)" % self.targets.count()
+        display = '%s: %s' % (self.type, self.value)
+        if self.artifact_relation.count() > 1:
+            display += " (%s)" % self.artifact_relation.count()
         return display
+
+    @property
+    def related_list(self):
+        targets = []
+        for artifact_relation in self.artifact_relation.all():
+            targets.append(artifact_relation.related)
+        return targets
 
 
 class ArtifactRelation(NgenModel):
-    artifact = models.ForeignKey('ngen.Artifact', on_delete=models.CASCADE, related_name='targets')
+    artifact = models.ForeignKey('ngen.Artifact', on_delete=models.CASCADE, related_name='artifact_relation')
     object_id = models.PositiveIntegerField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name='artifact_relation')
-    target = GenericForeignKey()
+    related = GenericForeignKey()
+
+    class Meta:
+        db_table = 'artifact_relation'
+        unique_together = ['artifact', 'object_id', 'content_type']
+
+    def __str__(self):
+        return "%s -> %s: %s" % (self.artifact, self.content_type.name, self.related)
 
 
 class ArtifactRelated(models.Model):
-    relations = GenericRelation('ngen.ArtifactRelation', related_query_name='%(class)ss')
+    artifact_relation = GenericRelation('ngen.ArtifactRelation', related_query_name='%(class)ss')
 
     class Meta:
         abstract = True
 
     @property
     def artifacts(self):
-        return Artifact.objects.filter(targets__in=self.artifact_relation.all())
+        return Artifact.objects.filter(artifact_relation__in=self.artifact_relation.all())
