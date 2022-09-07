17a4ec1fb8b94be66d5f94e2c03cc9827d68094a
dam <drubio@cert.unlp.edu.ar>
2022-09-07 19:02:47 -0300

    fixtures implementation for empty ngen instances
    Signed-off-by: dam <drubio@cert.unlp.edu.ar>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,6 @@
 python manage.py makemessages -l es -i venv --no-location
 python manage.py compilemessages -l es -i venv
 python manage.py migrate --noinput
-python manage.py createsuperuser --noinput --username ngen
 python manage.py collectstatic -c --noinput
+python manage.py  loaddata --app ngen priority feed tlp user taxonomy state edge report network_entity network contact
 exec "$@"
