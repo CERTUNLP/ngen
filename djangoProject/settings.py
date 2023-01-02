8f8d1d3f9d680e932eea90bbd8ed053ed62bc616
Mateo Durante <mateodurante@gmail.com>
2023-01-02 18:27:27 +0100

    cros origin credentials

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -313,6 +313,7 @@ if DEBUG:
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = os.environ.get('DJANGO_CORS_ALLOWED_ORIGINS').split(',')
 CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS').split(',')
 
