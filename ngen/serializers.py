7a34e1d287543b4a4306e890d58cf5ecab2efb0e
Mateo Durante <mateodurante@gmail.com>
2023-03-03 15:01:50 +0100

    login con username

--- ngen/serializers.py
+++ ngen/serializers.py
@@ -389,8 +389,9 @@ def _generate_jwt_token(user):
 
 
 class LoginSerializer(serializers.Serializer):
-    email = serializers.CharField(max_length=255)
-    username = serializers.CharField(max_length=255, read_only=True)
+    # email = serializers.CharField(max_length=255)
+    # username = serializers.CharField(max_length=255, read_only=True)
+    username = serializers.CharField(max_length=255)
     password = serializers.CharField(max_length=128, write_only=True)
 
     def validate(self, data):
