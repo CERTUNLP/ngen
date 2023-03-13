502bfe26802bf64cbaad1f6b77d08e7785f23c26
Mateo Durante <mateodurante@gmail.com>
2023-03-13 16:10:41 +0100

    default page size 10

--- ngen/apps.py
+++ ngen/apps.py
@@ -7,6 +7,6 @@ class NgenConfig(AppConfig):
     name = 'ngen'
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 50
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
