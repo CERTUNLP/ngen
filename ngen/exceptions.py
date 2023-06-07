eeba55139ee038a5a63a378d83a54862cbf7b365
Mateo Durante <mateodurante@gmail.com>
2023-06-07 16:42:42 +0200

    prevent model exceptions on rest django drf

--- ngen/exceptions.py
+++ ngen/exceptions.py
@@ -1,8 +1,10 @@
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError as core_ValidationError
 
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import exception_handler
 from rest_framework.response import Response
 from rest_framework import status
+from django.db import IntegrityError
 
 # https://github.com/encode/django-rest-framework/discussions/7850
 
@@ -12,7 +14,21 @@ def django_error_handler(exc, context):
     # to get the standard error response.
     response = exception_handler(exc, context)
 
-    if response is None and isinstance(exc, ValidationError):
+    if response is None and isinstance(exc, core_ValidationError):
         return Response(status=status.HTTP_400_BAD_REQUEST, data=exc.message_dict)
+
+    elif response is None and isinstance(exc, IntegrityError):
+        error_message = str(exc)
+        # duplicate key value violates unique constraint \"network_entity_slug_key\"\nDETAIL:  Key (slug)=(test) already exists.\n
+        if 'duplicate key value violates unique constraint' in error_message:
+            try:
+                key = error_message.split('Key (')[1].split(')')[0].strip()
+                value = error_message.split(')=(')[1].split(')')[0].strip()
+                return Response(status=status.HTTP_400_BAD_REQUEST, data={key: [f'Ya existe una entidad de red con {key}={value}.']})
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST, data={'error': [error_message]})
+        else:
+            # update or delete on table \"network_entity\" violates foreign key constraint \"network_network_entity_id_01d78cbb_fk_network_entity_id\" on table \"network\"\nDETAIL:  Key (id)=(2) is still referenced from table \"network\".\n
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={'error': [error_message]})
 
     return response
