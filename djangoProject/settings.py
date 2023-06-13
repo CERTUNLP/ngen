abd42d7e5be77b31a6fab3bbf817bdd7f3cecce7
dam <drubio@cert.unlp.edu.ar>
2023-06-13 13:38:08 -0300

    reverse proxy for frontend
    Signed-off-by: dam <drubio@cert.unlp.edu.ar>

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -151,7 +151,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     ),
 }
 
