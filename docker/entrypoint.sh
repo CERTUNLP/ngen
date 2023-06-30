a544cff7c1111911370af8e5c5b506afcef3839e
Mateo Durante <mateodurante@gmail.com>
2023-06-30 12:31:15 +0200

    avoid messages and django.po generation every time

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python manage.py makemessages -l es -i venv --no-location
+# python manage.py makemessages -l es -i venv --no-location
 python manage.py compilemessages -l es -i venv
 python manage.py migrate --noinput
 python manage.py collectstatic -c --noinput
