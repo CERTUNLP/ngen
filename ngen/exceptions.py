57b1bbf378ef45b5d2b55c45aabf743272428f7c
Mateo Durante <mateodurante@gmail.com>
2023-08-01 18:48:55 +0100

    more descriptive exceptions

--- ngen/exceptions.py
+++ ngen/exceptions.py
@@ -22,13 +22,14 @@ def django_error_handler(exc, context):
         # duplicate key value violates unique constraint \"network_entity_slug_key\"\nDETAIL:  Key (slug)=(test) already exists.\n
         if 'duplicate key value violates unique constraint' in error_message:
             try:
+                klass = context.get('view').queryset.model.__name__
                 key = error_message.split('Key (')[1].split(')')[0].strip()
                 value = error_message.split(')=(')[1].split(')')[0].strip()
-                return Response(status=status.HTTP_400_BAD_REQUEST, data={key: [f'Ya existe una entidad de red con {key}={value}.'], '_detail': error_message})
+                return Response(status=status.HTTP_400_BAD_REQUEST, data={key: [f'Ya existe una entidad {klass} con {key}={value}.'], '_detail': error_message})
             except:
                 return Response(status=status.HTTP_400_BAD_REQUEST, data={'error': [error_message]})
         elif 'update or delete on table' in error_message and 'violates foreign key constraint' in error_message:
-            return Response(status=status.HTTP_400_BAD_REQUEST, data={'__all__': [f'No se puede editar y/o eliminar la entidad de red porque hay objetos que dependen de ella.'], '_detail': error_message})
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={'__all__': [f'No se puede editar y/o eliminar la entidad porque hay objetos que dependen de ella.'], '_detail': error_message})
         else:
             # update or delete on table \"network_entity\" violates foreign key constraint \"network_network_entity_id_01d78cbb_fk_network_entity_id\" on table \"network\"\nDETAIL:  Key (id)=(2) is still referenced from table \"network\".\n
             return Response(status=status.HTTP_400_BAD_REQUEST, data={'error': [error_message]})
