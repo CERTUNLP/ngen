83aaa195e2e1d3e40037b307b31e6485711225e2
Mateo Durante <mateodurante@gmail.com>
2023-09-26 14:00:06 +0200

    complete refactor signals

--- ngen/models/__init__.py
+++ ngen/models/__init__.py
@@ -1,8 +1,6 @@
 from auditlog.registry import auditlog
 from django.apps import apps
 
-from django.contrib.auth import get_user_model
-
 from .administration import *
 from .announcement import *
 from .artifact import *
@@ -16,14 +14,3 @@ from .utils import *
 for model in apps.all_models['ngen'].values():
     if issubclass(model, NgenModel):
         auditlog.register(model)
-
-# User creation auth token
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
