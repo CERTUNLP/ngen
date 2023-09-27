64c338790bfc3ab55dfd4932b1749315d27a1d19
volframz <bristvak@gmail.com>
2023-09-27 19:08:46 -0300

    Fix typo

--- ngen/tests/api/test_administration.py
+++ ngen/tests/api/test_administration.py
@@ -1,4 +1,4 @@
-from ngen.models import NgenModel, User, administration, Priority, Tlp, Feed, config, settings
+from ngen.models import NgenModel, User, administration, Priority, Tlp, Feed, config
 from datetime import datetime, timedelta
 from django.urls import include, path, reverse
 from rest_framework.test import APITestCase, URLPatternsTestCase
