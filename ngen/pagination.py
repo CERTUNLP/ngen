420a0ea2c36bc7d7c7f40b7cc0aebb71d0a47c9f
Mateo Durante <mateodurante@gmail.com>
2024-01-04 11:36:17 +0100

    dynamic page size

--- ngen/pagination.py
+++ ngen/pagination.py
@@ -1,7 +1,14 @@
 from rest_framework.pagination import PageNumberPagination
+from constance import config
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 100
+
+    @property
+    def page_size(self):
+        return getattr(config, 'PAGE_SIZE', 10)
+
+    @property
+    def max_page_size(self):
+        return getattr(config, 'PAGE_SIZE_MAX', 100)
