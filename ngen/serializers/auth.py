3611a337deb59aaf2f8a5790f8ace406a381198d
Mateo Durante <mateodurante@gmail.com>
2024-01-22 13:11:59 +0100

    Added user data on login

--- ngen/serializers/auth.py
+++ ngen/serializers/auth.py
@@ -3,6 +3,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from rest_framework.reverse import reverse
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from ngen.models import User
 from .common.mixins import AuditSerializerMixin
@@ -87,3 +89,32 @@ class PermissionSerializer(AuditSerializerMixin):
     class Meta:
         model = Permission
         fields = '__all__'
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data_user = reverse('user-detail', kwargs={'pk': self.user.id}, request=self.context.get('request'))
+        data_priority = reverse('priority-detail', kwargs={'pk': self.user.priority.id},
+                                request=self.context.get('request'))
+        # Get all permissions from groups and user
+        perms = Permission.objects.filter(group__in=self.user.groups.all()) | self.user.user_permissions.all()
+        perm_serializer = PermissionSerializer(perms, many=True, context={'request': self.context.get('request')})
+
+        data.update({
+            'id': self.user.id,
+            'url': data_user,
+            'email': self.user.email,
+            'first_name': self.user.first_name,
+            'last_name': self.user.last_name,
+            'priority': data_priority,
+            'last_login': self.user.last_login,
+            'date_joined': self.user.date_joined,
+            'is_superuser': self.user.is_superuser,
+            'is_staff': self.user.is_staff,
+            'permissions': perm_serializer.data,
+        })
+
+        return data
