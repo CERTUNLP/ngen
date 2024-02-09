5966d3724d51d0d5d080e761d21c36c52d5bb074
Mateo Durante <mateodurante@gmail.com>
2024-02-09 12:21:37 -0300

    remove usage of case_template fixture

--- ngen/tests/models/test_cases.py
+++ ngen/tests/models/test_cases.py
@@ -3,7 +3,7 @@ Django Unit Tests for Case model
 """
 from django.test import TestCase
 
-from ngen.models import Tlp, Priority, CaseTemplate, State, Case
+from ngen.models import Tlp, Priority, CaseTemplate, State, Case, Taxonomy, Feed
 
 
 class CaseTest(TestCase):
@@ -12,8 +12,7 @@ class CaseTest(TestCase):
     """
 
     fixtures = ["priority.json", "tlp.json", "user.json", "state.json",
-                "feed.json", "taxonomy.json", "case_template.json"
-                ]
+                "feed.json", "taxonomy.json"]
 
     def setUp(self):
         """
@@ -23,16 +22,33 @@ class CaseTest(TestCase):
         self.priority = Priority.objects.get(slug="critical")
         self.tlp = Tlp.objects.get(slug="green")
         self.state = State.objects.get(slug="open")
-        self.case_template = CaseTemplate.objects.get(pk=1)
         self.name = 'Test Case'
+
+        self.template1 = CaseTemplate.objects.create(
+            domain="info.unlp.edu.ar",
+            priority=Priority.objects.get(slug="critical"),
+            event_taxonomy=Taxonomy.objects.get(slug="blacklist"),
+            event_feed=Feed.objects.get(slug="csirtamericas"),
+            case_tlp=Tlp.objects.get(slug="white"),
+            case_state=State.objects.get(slug="staging")
+        )
+        self.template2 = CaseTemplate.objects.create(
+            domain="alumnos.unlp.edu.ar",
+            priority=Priority.objects.get(slug="high"),
+            event_taxonomy=Taxonomy.objects.get(slug="botnet"),
+            event_feed=Feed.objects.get(slug="bro"),
+            case_tlp=Tlp.objects.get(slug="green"),
+            case_state=State.objects.get(slug="staging")
+        )
 
         self.case = Case.objects.create(
             priority=self.priority,
             tlp=self.tlp,
-            casetemplate_creator=self.case_template,
+            casetemplate_creator=self.template1,
             state=self.state,
             name=self.name
         )
+
 
     def test_case_creation(self):
         """
@@ -56,7 +72,7 @@ class CaseTest(TestCase):
         """
         This will test Case casetemplate_creator attribute
         """
-        self.assertEqual(self.case.casetemplate_creator, self.case_template)
+        self.assertEqual(self.case.casetemplate_creator, self.template1)
 
     def test_state(self):
         """
@@ -69,3 +85,17 @@ class CaseTest(TestCase):
         This will test Case name attribute
         """
         self.assertEqual(self.case.name, self.name)
+
+    def validate_unique_case_tempalte(self):
+        """
+        This will test Case casetemplate_creator attribute
+        """
+        with self.assertRaises(Exception):
+            CaseTemplate.objects.create(
+                domain="info.unlp.edu.ar",
+                priority=Priority.objects.get(slug="critical"),
+                event_taxonomy=Taxonomy.objects.get(slug="blacklist"),
+                event_feed=Feed.objects.get(slug="csirtamericas"),
+                case_tlp=Tlp.objects.get(slug="white"),
+                case_state=State.objects.get(slug="staging")
+            )
