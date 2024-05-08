cf708652ff7c0311372c06ceb8b73ffc253a0d66
Mateo Durante <mateodurante@gmail.com>
2024-05-08 17:01:03 +0200

    Added search filters and ordering to Reports, Todo and Tasks

--- ngen/views/taxonomy.py
+++ ngen/views/taxonomy.py
@@ -35,18 +35,40 @@ class PlaybookViewSet(viewsets.ModelViewSet):
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = models.Task.objects.all()
+    filter_backends = [
+        filters.SearchFilter,
+        django_filters.rest_framework.DjangoFilterBackend,
+        filters.OrderingFilter
+    ]
+    search_fields = ['name', 'description']
+    ordering_fields = ['id', 'created', 'modified', 'name', 'playbook', 'priority']
     serializer_class = serializers.TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class TodoTaskViewSet(viewsets.ModelViewSet):
     queryset = models.TodoTask.objects.all()
+    filter_backends = [
+        filters.SearchFilter,
+        django_filters.rest_framework.DjangoFilterBackend,
+        filters.OrderingFilter
+    ]
+    search_fields = ['note', 'assigned_to__username']
+    ordering_fields = ['id', 'created', 'modified', 'completed', 'assigned_to', 'note']
     serializer_class = serializers.TodoTaskSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class ReportViewSet(viewsets.ModelViewSet):
     queryset = models.Report.objects.all()
+    filter_backends = [
+        filters.SearchFilter,
+        django_filters.rest_framework.DjangoFilterBackend,
+        filters.OrderingFilter
+    ]
+    search_fields = ['taxonomy__name']
+    ordering_fields = ['id', 'created', 'modified', 'problem', 'derived_problem', 'taxonomy', 'verification',
+                       'recommendations', 'more_information', 'lang']
     serializer_class = serializers.ReportSerializer
     permission_classes = [permissions.IsAuthenticated]
 
