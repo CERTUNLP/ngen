6ecaf15a36a6b50be774e1b2d1493c01cbd65ba4
Mateo Durante <mateodurante@gmail.com>
2024-07-10 12:11:42 +0200

    Fix errores template

--- frontend/src/api/services/profile.js
+++ frontend/src/api/services/profile.js
@@ -1,17 +1,18 @@
-import  apiInstance  from "../api";
+import apiInstance from "../api";
 import { COMPONENT_URL } from '../../config/constant';
 import setAlert from '../../utils/setAlert';
 
 const getProfile = () => {
-    let messageError = `No se ha recuperado la informacion del reporte. `;
+    let messageError = `No se ha recuperado la informacion del usuario. `;
     return apiInstance.get(COMPONENT_URL.profile)
-    .then(response => {        
-        return response;
-    }).catch( error => { 
-        let statusText = error.response.statusText;
-        messageError += statusText;
-        setAlert(messageError , "error", "report");
-        return Promise.reject(error);
-    });
+        .then(response => {
+            return response;
+        }).catch(error => {
+            let statusText = error.response.statusText;
+            messageError += statusText;
+            setAlert(messageError, "error", "report");
+            return Promise.reject(error);
+        });
 }
+
 export { getProfile }
