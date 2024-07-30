fe1e4a39b09cb17b6c81bbdf3818585eaa2228d9
Agustin Paturlanne <50930830+aguspatur22@users.noreply.github.com>
2024-07-30 06:04:05 -0300

    Add warning details for merge modal confirmation (#134)

--- frontend/src/components/Modal/ModalConfirm.js
+++ frontend/src/components/Modal/ModalConfirm.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
+import { getTaxonomy } from '../../api/services/taxonomies';
 
 const ModalConfirm = (props) => { // props: showModal, onHide, ifConfirm, type, component, state, name
+    const [warning, setWarning] = useState('');
+    const [eventOrCase, setEventOrCase] = useState('');
+
+    useEffect(() => {
+        let names = Object.values(props.name);
+        const eventOrCase = names.some((el) => el.includes("event")) ? "eventos" : "casos";
+        setEventOrCase(eventOrCase);
+    }, [props.name]);
+
+    useEffect(() => {
+        if (props.type === "merge" && eventOrCase === "eventos") {
+            const fetchTaxonomies = async () => {
+                try {
+                    let names = Object.values(props.name);
+                    // Fetch event data
+                    const eventData = await Promise.all(names.map(url => getTaxonomy(url.trim())));
+                    const taxonomyUrls = eventData.map(event => event.data.taxonomy);
+                    // Fetch Domain or Cidr data
+                    const dOrCidr = eventData.map(event => event.data.domain ? event.data.domain : event.data.cidr);
+                    // Fetch taxonomy data
+                    const taxonomyData = await Promise.all(taxonomyUrls.map(url => getTaxonomy(url.trim())));
+                    const taxonomyNames = taxonomyData.map(data => data.data.name);
+                    const diffTax = (new Set(taxonomyNames).size > 1);
+                    const diffDOrCidr = (new Set(dOrCidr).size > 1);
+                    const warning = diffTax && diffDOrCidr 
+                        ? "(Advertencia: las taxonomias y domain/cidr de los eventos son diferentes)"
+                        : diffTax 
+                        ? "(Advertencia: las taxonomias de los eventos son diferentes)" 
+                        : diffDOrCidr 
+                        ? "(Advertencia: los domain/cidr de los eventos son diferentes)" 
+                        : "";
+                    setWarning(warning);
+                } catch (error) {
+                    console.error('Error fetching taxonomies:', error);
+                }
+            };
+
+            fetchTaxonomies();
+        }
+    }, [props.name, props.type, eventOrCase]);
 
     const type = {
         editState:
@@ -21,8 +62,7 @@ const ModalConfirm = (props) => { // props: showModal, onHide, ifConfirm, type, 
         merge:
         {
             header: `${props.component ? `Merge de ${props.component}` : 'Merge'}`,
-            //message: `${`¿Desea mergear los casos: ${props.name}?`}`,
-            message: `${`¿Desea mergear los casos seleccionados?`}`,
+            message: `¿Desea mergear los ${eventOrCase} seleccionados? ${warning}`,
             variantButtonConfirm: 'outline-primary',
             textButtonConfirm: 'Aceptar'
         },
