d7c8b03eecfccea39b1d3248296dd189d74804ad
Mateo Durante <mateodurante@gmail.com>
2024-08-06 01:03:18 +0200

    hotfix dashboard events_count

--- frontend/src/views/dashboard/DashDefault/chart/FeedGraph.js
+++ frontend/src/views/dashboard/DashDefault/chart/FeedGraph.js
@@ -7,7 +7,7 @@ const FeedGraph = ({ dashboardFeed }) => {
     const [feed, setFeed] = useState([]);
 
     const filtrarEventosNoCero = (datos) => { //opino que se debe mostrar aquellas fuentes que este asociados a uno amas eventos 
-        return datos.filter((objeto) => objeto.event_count !== 0);
+        return datos.filter((objeto) => objeto.events_count !== 0);
     };
 
     useEffect(() => {
@@ -19,7 +19,7 @@ const FeedGraph = ({ dashboardFeed }) => {
     return (
         <div>
             {
-                feed.length > 0 ? <NVD3Chart id="chart" height={600} type="pieChart" datum={feed} x="feed_name" y="event_count" donut labelType="percent" /> :
+                feed.length > 0 ? <NVD3Chart id="chart" height={600} type="pieChart" datum={feed} x="feed_name" y="events_count" donut labelType="percent" /> :
                     t('feeds_noEventsAssociated')
 
             }
