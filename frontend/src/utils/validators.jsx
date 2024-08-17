f8187bdef1f396093561901398caac501416786d
Mateo Durante <mateodurante@gmail.com>
2024-08-17 14:44:36 +0200

    fix url validator

--- frontend/src/utils/validators.jsx
+++ frontend/src/utils/validators.jsx
@@ -56,15 +56,7 @@ const validateNumbers = (text) => {
 };
 
 const validateURL = (str) => {
-  var pattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
-    'i'
-  ); // fragment locator
+  var pattern = /\bhttps?:\/\/[^\s/$.?#].[^\s]*\b/i;
   return !!pattern.test(str);
 };
 const validateHours = (hours) => {
