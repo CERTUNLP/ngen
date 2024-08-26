883719d437a3757ec50eeedf15a884d2ad75a610
Bar <142248289+volframz@users.noreply.github.com>
2024-08-26 12:56:08 -0300

    Fix/i18n (#162)
    * Changes to dockerfile and docker-compose to allow correct passing of env variables to react
    
    * Fix syntax issue
    
    * Add API Endpoint to retrieve ngen language + i18n config
    
    * Undo env var at docker file
    
    * Undo change docker-compose.yml
    
    ---------
    
    Co-authored-by: Mateo Durante <mdurante@cert.unlp.edu.ar>

--- project/urls.py
+++ project/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 import comment.api.views as comment_views
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -57,9 +58,9 @@ router.register(r'constance', views.ConstanceViewSet, basename='constance')
 router.register(r'stringidentifier', views.StringIdentifierViewSet, basename='stringidentifier')
 # router.register(r"comments", views.CommentViewSet)
 router.register(r"register", views.RegisterViewSet, basename="register")
-router.register(r'groups', views.GroupViewSet, basename='group')
-router.register(r'permission', views.PermissionViewSet, basename='permission')
-router.register(r'contenttype', views.ContentTypeViewSet, basename='contenttype')
+router.register(r"groups", views.GroupViewSet, basename="group")
+router.register(r"permission", views.PermissionViewSet, basename="permission")
+router.register(r"contenttype", views.ContentTypeViewSet, basename="contenttype")
 # router.register(r"login", views.LoginViewSet, basename="login")
 router.register(r'minified/taxonomy', views.TaxonomyMinifiedViewSet, basename='minified-taxonomy')
 router.register(r'minified/taxonomygroup', views.TaxonomyGroupMinifiedViewSet, basename='minified-taxonomygroup')
@@ -72,60 +73,50 @@ router.register(r'minified/contact', views.ContactMinifiedViewSet, basename='min
 router.register(r'minified/user', views.UserMinifiedViewSet, basename='minified-user')
 router.register(r'minified/case', views.CaseMinifiedViewSet, basename='minified-case')
 router.register(r'minified/artifact', views.ArtifactMinifiedViewSet, basename='minified-artifact')
+router.register(r"ngenconfig", views.SettingsViewSet, basename="settings")
+
 if settings.ELASTIC_ENABLED:
     from ngen.documents import CaseDocumentViewSet
 
-    router.register(r'search/case', CaseDocumentViewSet, basename='casedocument')
+    router.register(r"search/case", CaseDocumentViewSet, basename="casedocument")
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/comment/', comment_views.CommentList.as_view(), name='comment-list'),
-    path('api/comment/create/', comment_views.CommentCreate.as_view(), name='comment-create'),
-    path('api/comment/<int:pk>/', comment_views.CommentDetail.as_view(), name='comment-detail'),
-    path('api/user/<int:pk>/audit/', views.tools.UserAuditsListView.as_view({'get': 'list'}),
-         name='user-logentry-list'),
-    path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token-create'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token-verify'),
-    path('api/token/simple/', authtokenviews.obtain_auth_token, name='token-simple'),
-    path('api/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/logout/', views.LogoutView.as_view(), name="logout"),
-    path('api/ctoken/', views.CookieTokenObtainPairView.as_view(), name='ctoken-create'),
-    path('api/ctoken/refresh/', views.CookieTokenRefreshView.as_view(), name='ctoken-refresh'),
-    path('api/ctoken/logout/', views.CookieTokenLogoutView.as_view(), name='ctoken-logout'),
-    path('api/about/', views.AboutView.as_view(), name='about'),
-    path('__debug__/', include('debug_toolbar.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
+    path("api/admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/comment/", comment_views.CommentList.as_view(), name="comment-list"),
+    path("api/comment/create/", comment_views.CommentCreate.as_view(), name="comment-create"),
+    path("api/comment/<int:pk>/", comment_views.CommentDetail.as_view(), name="comment-detail"),
     path(
-        'api/dashboard/events',
-        views.dashboards.events.DashboardEventsView.as_view(),
-        name='dashboard-events'
+        "api/user/<int:pk>/audit/", views.tools.UserAuditsListView.as_view({"get": "list"}), name="user-logentry-list"
     ),
+    path("api/token/", views.CustomTokenObtainPairView.as_view(), name="token-create"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token-verify"),
+    path("api/token/simple/", authtokenviews.obtain_auth_token, name="token-simple"),
+    path("api/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api/logout/", views.LogoutView.as_view(), name="logout"),
+    path("api/ctoken/", views.CookieTokenObtainPairView.as_view(), name="ctoken-create"),
+    path("api/ctoken/refresh/", views.CookieTokenRefreshView.as_view(), name="ctoken-refresh"),
+    path("api/ctoken/logout/", views.CookieTokenLogoutView.as_view(), name="ctoken-logout"),
+    path("api/about/", views.AboutView.as_view(), name="about"),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("api/dashboard/events", views.dashboards.events.DashboardEventsView.as_view(), name="dashboard-events"),
+    path("api/dashboard/cases", views.dashboards.cases.DashboardCasesView.as_view(), name="dashboard-cases"),
+    path("api/dashboard/feeds", views.dashboards.feeds.DashboardFeedsView.as_view(), name="dashboard-feeds"),
     path(
-        'api/dashboard/cases',
-        views.dashboards.cases.DashboardCasesView.as_view(),
-        name='dashboard-cases'
-    ),
-    path(
-        'api/dashboard/feeds',
-        views.dashboards.feeds.DashboardFeedsView.as_view(),
-        name='dashboard-feeds'
-    ),
-    path(
-        'api/dashboard/network_entities',
+        "api/dashboard/network_entities",
         views.dashboards.network_entities.DashboardNetworkEntitiesView.as_view(),
-        name='dashboard-network-entities'
+        name="dashboard-network-entities",
     ),
 ]
 
 if not settings.ELASTIC_ENABLED:
-    urlpatterns += [re_path(r'^api/search', views.DisabledView.as_view(), name='disabled_view')]
+    urlpatterns += [re_path(r"^api/search", views.DisabledView.as_view(), name="disabled_view")]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
