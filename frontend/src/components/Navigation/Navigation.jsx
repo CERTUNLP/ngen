3852dadfe57cd9062909df973f5384f08826aaf1
Mateo Durante <mateodurante@gmail.com>
2024-08-27 03:12:40 +0200

    navigation without reloading all page

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Breadcrumb } from "react-bootstrap";
 
 const Navigation = ({ actualPosition, path = false, index = "" }) => {
   return (
     <React.Fragment>
       <Breadcrumb>
-        <Breadcrumb.Item href="./app/dashboard/default">
-          <i className="fas fa-home" />
+        <Breadcrumb.Item>
+          <Link to="/app/dashboard/default">
+            <i className="fas fa-home" />
+          </Link>
         </Breadcrumb.Item>
-        {path ? <Breadcrumb.Item href={path}>{index}</Breadcrumb.Item> : ""}
+        {path ? (
+          <Breadcrumb.Item>
+            <Link to={path}>{index}</Link>
+          </Breadcrumb.Item>
+        ) : (
+          ""
+        )}
         <Breadcrumb.Item active>
           <b>{actualPosition}</b>
         </Breadcrumb.Item>
