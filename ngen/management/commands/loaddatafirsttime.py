3373ccff9eba4d3194f1f3b1158fbd58a23f6522
Mateo Durante <mateodurante@gmail.com>
2024-10-01 19:44:55 +0200

    hotfix misstypo

--- ngen/management/commands/loaddatafirsttime.py
+++ ngen/management/commands/loaddatafirsttime.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
 from django.db.utils import IntegrityError
 from ngen.models import User
@@ -7,15 +7,20 @@ from ngen.models import User
 class Command(BaseCommand):
     help = "Same as loaddata, but only runs if the User table is empty."
 
-    def handle(self, *args, **kwargs):
+    def add_arguments(self, parser):
+        parser.add_argument("fixtures", nargs="+", help="List of fixture files to load")
 
+    def handle(self, *args, **options):
         if User.objects.exists():
             self.stdout.write(
                 self.style.WARNING("User data already exists. Skipping loaddata.")
             )
         else:
             try:
-                call_command("loaddata", *args, **kwargs)
+                fixtures = options["fixtures"]
+                call_command("loaddata", *fixtures)
                 self.stdout.write(self.style.SUCCESS("Data loaded successfully."))
             except IntegrityError as e:
                 self.stdout.write(self.style.ERROR(f"Error loading data: {e}"))
+            except CommandError as e:
+                self.stdout.write(self.style.ERROR(f"Command error: {e}"))
