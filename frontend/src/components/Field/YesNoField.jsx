b01a987c927d1f84f2fb1675973269881d4e7b1b
Mateo Durante <mateodurante@gmail.com>
2024-10-13 14:20:50 +0200

    removed unused lib

--- frontend/src/components/Field/YesNoField.jsx
+++ frontend/src/components/Field/YesNoField.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
 const YesNoField = ({ value }) => {
