2a1b86858e004a6d9e9653fc5899393af1a2d29b
Mateo Durante <mateodurante@gmail.com>
2025-01-02 12:28:25 +0100

    Refactor develop and production environment configuration

--- frontend/nginx/docker-entrypoint.sh
+++ frontend/nginx/docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Usar envsubst para sustituir variables en la configuración de NGINX
-envsubst '$VITE_APP_API_SERVER' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+export VITE_APP_API_SERVER=${VITE_APP_API_SERVER:-/api/}
+envsubst '\$VITE_APP_API_SERVER' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Arrancar NGINX
 nginx -g 'daemon off;'
--- frontend/public/env.js
+++ frontend/public/env.js
@@ -1,1 +1,1 @@
-localStorage.setItem("API_SERVER", "http://localhost:3000/api");
+localStorage.setItem("API_SERVER", "/api/");
