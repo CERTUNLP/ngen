581d3757cb6bf86ce71c473c25202a94f14ebc1c
Mateo Durante <mateodurante@gmail.com>
2026-05-04 23:39:01 +0200

    avoid raise condition con sending email inside atomic transaction

--- ngen/tasks.py
+++ ngen/tasks.py
@@ -441,6 +441,7 @@ def async_send_email(self, email_message_id: int):
         email_message = ngen.models.EmailMessage.objects.get(id=email_message_id)
     except ngen.models.EmailMessage.DoesNotExist as e:
         if self.request.retries == self.max_retries:
+            logger.error(f"EmailMessage {email_message_id} not found after {self.max_retries} retries.")
             return {"status": "error", "message": f"Email {email_message_id} not found"}
 
         exponential_backoff = (self.request.retries + 1) ** 2
