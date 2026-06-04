3db4660fc27e6a5ac2ba45b9f2dc233ca9cab8cc
Ngen Developer <dev@ngen.local>
2026-06-04 15:38:14 +0200

    fix compilemessages on dev

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# python manage.py makemessages -l es -i venv --no-location
-python manage.py compilemessages -l es -i venv
+if [ "$DJANGO_DEBUG" = "True" ]; then
+    find . -name "*.mo" -delete
+    python manage.py compilemessages -l es -i venv
+fi
 python manage.py migrate --noinput
 python manage.py collectstatic -c --noinput
 python manage.py loaddatafirsttime priority feed tlp user taxonomy state edge report_en report_es network_entity network contact playbook group
